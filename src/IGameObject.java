be4b3eb84bac7fa3cbaaf6fb102e33678e809256
marianasafonso <marianasafonso99@gmail.com>
2025-04-25 14:37:57 +0100

    IGameObject.java

--- src/IGameObject.java
+++ src/IGameObject.java
@@ -10,7 +10,17 @@ public interface IGameObject {
     ITransform transform();
 
     /**
+     * @return the visual shape of the GameObject used by the GUI
+     */
+    IShape shape();
+
+    /**
      * @return the Collider of the GameObject with its centroid at this.transform().position()
      */
     ICollider collider();
+
+    /**
+     * @return the Behaviour of the GameObject, which defines how it reacts in the game (ex: move, shoot, flee, patrol)
+     */
+    IBehaviour behaviour();
 }
